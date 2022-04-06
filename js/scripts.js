ecdbc64816318eb3d8a6f55900291116fbf6da1b
OLGON92 <ogon318@gmail.com>
2022-04-05 20:54:52 -0700

    add files again because initial repo was not configured properly
    Co-authored-by: Rosario Ruvalcaba <mariarruval@gmail.com>
    Co-authored-by:Arthur Ortiz <Ortiz.Arthur.E@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,26 @@ $(document).ready(function() {
     const gender = $("input#gender").val();
     const zodiac = $("input#zodiac").val();
 
-    if ( age <= 18) {
-      $().hid
-      $(#kool).show(1000);
-    } else if ( gender === female && age >= 18 && age <= 35 && zodiac === "pisces" ) {
-      $(#ryan).show(1000);
+    if ( age <= 18 ) {
+      $(#dolly).hide(1000);
+      $(#ryan).hide(1000);
+      $(#ramones).hide(1000);
+      $(#kool).show(1000);  //<< SHOW
+    } else if ( gender === "female" && age >= 18 && age <= 35 && zodiac === "pisces" ) {
+      $(#dolly).hide(1000);
+      $(#ryan).show(1000);  //<< SHOW
+      $(#ramones).hide(1000);
+      $(#kool).hide(1000);
+    } else if ( gender === "male" && age >= 30 && zodiac === "virgo" ) {
+      $(#dolly).show(1000); //<< SHOW
+      $(#ryan).hide(1000); 
+      $(#ramones).hide(1000);
+      $(#kool).hide(1000);
+    } else ( gender === "male" || gender === "female" && age >= 18) {
+      $(#dolly).hide(1000);
+      $(#ryan).hide(1000); 
+      $(#ramones).show(1000); //<< SHOW
+      $(#kool).hide(1000);
     }
 
   });
